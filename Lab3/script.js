5e744625e389fa33a8de977c78189e2163c696cd
Vostok17 <gagarin.artem.2008@gmail.com>
2022-01-12 02:52:01 +0300

    try to write

--- Lab3/script.js
+++ Lab3/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const insertItem = document.querySelector('.insert-item');
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+    const result = await fetch('https://web-lab3k.herokuapp.com/v1/graphql', {
+        method: 'POST',
+        body: JSON.stringify({
+            query: operationsDoc,
+            variables,
+            operationName,
+        }),
+    });
+    return result.json();
+}
+
+const addItem = `mutation MyMutation($date: String = "") {
+    insert_Dates_one(object: {date: $date}) {
+      id
+    }
+}`;
+
+const sendItem = async event => {
+    event.preventDefault();
+
+    const date = insertItem.elements.date;
+    await fetchGraphQL(addItem, 'MyMutation', {
+        date,
+    });
+};
+
+insertItem.addEventListener('submit', sendItem);
