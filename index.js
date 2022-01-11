83f4cb92bff9044f687a469bb05d4d09805c3ed8
Vostok17 <gagarin.artem.2008@gmail.com>
2022-01-11 21:09:04 +0300

    try to run

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-console.log('server working');
-console.log('hi');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello API');
+});
+
+app.listen(3012, () => {
+    console.log('API app started');
+});
